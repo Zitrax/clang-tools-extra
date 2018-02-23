8b3e769fea92e2f34b826506fe9ba8dd96cd61c2
Daniel Bengtsson <daniel@bengtssons.info>
2018-02-23 23:20:24 +0100

    fixup! Add new printf check (so far just to see how it works)
    Passing tests

--- test/clang-tidy/misc-printf-define.cpp
+++ test/clang-tidy/misc-printf-define.cpp
@@ -1,13 +1,15 @@
 // RUN: %check_clang_tidy %s misc-printf-define %t
 
+int printf(const char*){}
+
 void f(void) {
   printf("hello");
   // CHECK-MESSAGES: :[[@LINE-1]]:11: warning: printf string should start with capital letter [misc-printf-define]
   // CHECK-FIXES: {{^  }}printf("Hello");{{$}}
   #define LC "lc"
   printf(LC);
-  // CHECK-MESSAGES: :[[@LINE-1]]:11: warning: printf string should start with capital letter [misc-printf-define]
-  // CHECK-FIXES: {{^  }}printf("Lc");{{$}}
+  // CHECK-MESSAGES: :[[@LINE-1]]:10: warning: printf string should start with capital letter [misc-printf-define]
+  // CHECK-FIXES: {{^  }}#define LC "Lc"{{$}}
   printf(0);
   printf("Message");
   // FIXME: The below will currently fail since the FixItHint does not currently apply to defines
